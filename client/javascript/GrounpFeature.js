67d974fdde504f3c5936877ca6a51b0fb49a2c61
ninhnguyen2623 <ninh1160@gmail.com>
2023-03-29 21:14:40 +0700

    render-api

---
+++ client/javascript/GrounpFeature.js
@@ -0,0 +1,166 @@
+$(document).ready(function() {
+  $.ajax({
+      type:"GET",
+      url: "http://localhost:3000/api/category/list",
+      success: function(data){
+        let inHTML = " ";
+        data.forEach(function(item,index){
+          console.log(index);
+
+           if(index <= 7){
+            inHTML += `
+            <div class="container-grounpfreature">
+            <div class="title-grounpfeature" style=" background: linear-gradient(to left top, rgb(244, 248, 136), green);">
+                   <h3 class="fs-4  text-uppercase">${item.name}</h3>
+                 </div>
+            <div class="row pb-2 px-3 grounp-category" data=${item.category_id}>
+            
+              
+            </div>
+          </div>
+          <div class=" see-more-container my-2">
+            <div class="see-more-grounpfeature">
+              <a href="#"> Xem thêm sản phẩm...</a>
+            </div>
+          </div>
+            `
+           }
+        });
+        // console.log(inHTML);
+        $(".myGrounpFeature1").html(inHTML);
+        const categr = $(".grounp-category");
+         for(let i = 0; i < 8; i++) {
+          let category_id = $(categr[i]).attr("data");
+          let textTO = " ";
+          $.ajax({
+            type: "GET",
+            url: `http://localhost:3000/api/category/${category_id}`,
+            success: function(data){
+                let textHTML =" ";
+                data.products.forEach(function(i){
+                  // console.log(i);
+                  textHTML = ` 
+                  <div class="col-md-3 col-sm-6 col-6 p-0 ">
+                  <div class="product-box">
+                    <div class="product-inner-box position-relative">
+                      <div class="icons position-absolute">
+                        <a href="#" class="text-decoration-none"><i class="fa-solid fa-eye"></i></a>
+                        <a href="#" class="text-decoration-none"><i class="fa-solid fa-cart-plus"></i></a>
+                      </div>
+                      <div class="onsale position-absolute top-0 start-0">
+                        <span class="badge rounded-0"><i class="fa-solid fa-arrow-down"></i> 45%</span>
+                      </div>
+                      <div class="product-img">
+                        <img src="http://localhost:3000/img/products/${i.img}" alt="woodan chair" class="img-fluid">
+                      </div>
+                      <div class="cart-btn">
+                        <button class="btn btn-white bg-white shadow-sm rounded-pill" data="${i.product_id}"><i class="fa-solid fa-cart-shopping"></i>Mua Ngay</button>
+                      </div>
+                    </div>
+                    <div class="product-info">
+                      <div class="product-name">
+                        <h3>${i.name}</h3>
+                      </div>
+                      <div class="product-price">
+                        <span>${i.price} </span>đ
+                      </div>
+                    </div>
+                  </div>
+                  </div> 
+                  `
+                  textTO += textHTML;
+                });
+                categr[i].innerHTML = textTO;
+              }
+            })
+         }
+
+      }
+    })
+  });
+  // grounpcategory_2-------------------------------------------------------------------------------------------------
+  //-------------------------------------------------------------------------------------------------------------------
+  $(document).ready(function() {
+    $.ajax({
+        type:"GET",
+        url: "http://localhost:3000/api/category/list",
+        success: function(data){
+          let inHTML = " ";
+          data.forEach(function(item,index){
+             if(index > 7){
+              inHTML += `
+              <div class="container-grounpfreature-2">
+              <div class="title-grounpfeature-2">
+                  <div class="heart-grounpfeature-2"></div>
+                  <h3 class="fs-4">${item.name}</h3>
+              </div>
+              <div class="row pb-2 px-3 grounp-category2" data=${item.category_id}>
+              
+                
+              </div>
+            </div>
+            <div class=" see-more-container my-2">
+              <div class="see-more-grounpfeature">
+                <a href="#"> Xem thêm sản phẩm...</a>
+              </div>
+            </div>
+              `
+             }
+          });
+          // console.log(inHTML);
+          $(".myGrounpFeature").html(inHTML);
+
+          const categr2 = $(".grounp-category2");
+           for(let i = 0; i < 6; i++) {
+            console.log(categr2[i]);
+            
+              let category_id = $(categr2[i]).attr("data");
+              let textTO = " ";
+            $.ajax({
+              type: "GET",
+              url: `http://localhost:3000/api/category/${category_id}`,
+              success: function(data){
+                  let textHTML =" ";
+                  data.products.forEach(function(i,index1){
+                    // console.log(i);
+                      textHTML = ` 
+                      <div class="col-md-3 col-sm-6 col-6 p-0 ">
+                      <div class="product-box">
+                        <div class="product-inner-box position-relative">
+                          <div class="icons position-absolute">
+                            <a href="#" class="text-decoration-none"><i class="fa-solid fa-eye"></i></a>
+                            <a href="#" class="text-decoration-none"><i class="fa-solid fa-cart-plus"></i></a>
+                          </div>
+                          <div class="onsale position-absolute top-0 start-0">
+                            <span class="badge rounded-0"><i class="fa-solid fa-arrow-down"></i> 45%</span>
+                          </div>
+                          <div class="product-img">
+                            <img src="http://localhost:3000/img/products/${i.img}" alt="woodan chair" class="img-fluid">
+                          </div>
+                          <div class="cart-btn">
+                            <button class="btn btn-white bg-white shadow-sm rounded-pill" data="${i.product_id}"><i class="fa-solid fa-cart-shopping"></i>Mua Ngay</button>
+                          </div>
+                        </div>
+                        <div class="product-info">
+                          <div class="product-name">
+                            <h3>${i.name}</h3>
+                          </div>
+                          <div class="product-price">
+                            <span>${i.price} </span>đ
+                          </div>
+                        </div>
+                      </div>
+                      </div> 
+                      `
+                      textTO += textHTML;
+                    
+                  });
+                  categr2[i].innerHTML = textTO;
+                }
+              })
+            
+           }
+  
+        }
+      })
+    });
--- client/javascript/product.js
+++ client/javascript/product.js
@@ -1,26 +1,44 @@
-const cards = document.querySelectorAll( `.product-box`);
-[...cards].forEach((card) => {
-    card.addEventListener('mouseover',function(){
-        card.classList.add(`is-hover`);
+$(document).ready(function() {
+    $.ajax({
+        type:"GET",
+        url: "http://localhost:3000/api/product/list",
+        success: function(data){
+            html = "";
+            data.forEach(function(item,index){
+               if(index < 16){
+                html += `
+                <div class="col-md-3 col-sm-6 col-6 p-0 ">
+                     <div class="product-box">
+                       <div class="product-inner-box position-relative">
+                         <div class="icons position-absolute">
+                           <a href="#" data="${item.product_id}" class="text-decoration-none"data-toggle="modal" data-target="#modalId"><i class="fa-solid fa-eye"></i></a>
+                           <a href="#" class="text-decoration-none"><i class="fa-solid fa-cart-plus"></i></a>
+                         </div>
+                         <div class="onsale position-absolute top-0 start-0">
+                           <span class="badge rounded-0"><i class="fa-solid fa-arrow-down"></i> 45%</span>
+                         </div>
+                         <div class="product-img">
+                           <img src="http://localhost:3000/img/products/${item.img}" alt="woodan chair" class="img-fluid">
+                         </div>
+                         <div class="cart-btn">
+                           <button data=${item.product_id} class="btn btn-white bg-white shadow-sm rounded-pill"><i class="fa-solid fa-cart-shopping"></i>Mua Ngay</button>
+                         </div>
+                       </div>
+                       <div class="product-info">
+                         <div class="product-name">
+                           <h3>${item.name}</h3>
+                         </div>
+                         <div class="product-price">
+                           <span>${item.price}</span>đ
+                         </div>
+                       </div>
+                     </div>
+                     </div>
+                `
+               }
+            });
+            $(".product-list").html(html);
+        }
     })
-    card.addEventListener('mouseleave',function(){
-        card.classList.remove(`is-hover`);
-    })
-})
-// $(document).ready(function(){
-//     $(".card-link").click(function(){
-//         $(".rounded").attr("src",$(this).parent().siblings().attr("Src"));
-//         $(".product_name").text($(this).siblings("h3").text());
-//         $(".prpduct-desc").text($(this).siblings("div").find("p:nth(0").text());
-//         $(".product_price").text($(this).siblings("div").find("p:nth(1)").text());
-//     });
-// $(window).resize(function(){
-//     if($(window).width() <600) 
-//    {
-//      $(".modal-content").css("transform", "scaleX(1)");
-//     }
-//     else {
-//         $(".modal-content").css("transform", "scaleX(1.4)");
-//     }
-// });
-// })
+
+});
